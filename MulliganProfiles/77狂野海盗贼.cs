be48c7faa839c85a2f12e645aaa4da3e8eebce9b
xnyld <857343134@qq.com>
2022-04-15 12:53:03 +0800

    海盗贼

---
+++ MulliganProfiles/77狂野海盗贼.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SmartBot.Database;
+using SmartBot.Plugins.API;
+// ### 潜行者
+// # 职业：潜行者
+// # 模式：狂野模式
+// #
+// # 2x (1) 南海船工
+// # 2x (1) 奖品掠夺者
+// # 2x (1) 影袭
+// # 1x (1) 海盗帕奇斯
+// # 2x (1) 秘密通道
+// # 2x (1) 致命药膏
+// # 2x (1) 锈水海盗 CORE_AT_029 
+// # 2x (1) 鱼排斗士 TSC_963
+// # 2x (2) 洞穴探宝者 LOOT_033
+// # 2x (2) 空降歹徒 DRG_056
+// # 2x (2) 船载火炮 GVG_075 
+// # 2x (2) 雾帆劫掠者
+// # 2x (3) 剑鱼 TSC_086
+// # 2x (4) 恐怖海盗 VAN_NEW1_022
+// # 2x (5) 劈砍课程 SCH_623
+// # 1x (6) 重拳先生
+// # 
+// AAEBAbfyBAKRvAK/gAQO1AXuBvsP5dEC6bADqssD99QD890DpooEkp8ElJ8EiskE/dMEmtsEAA==
+// # 
+// # 想要使用这副套牌，请先复制到剪贴板，然后在游戏中点击“新套牌”进行粘贴。
+
+namespace SmartBot.Mulligan
+{
+    [Serializable]
+    public class DefaultMulliganProfile : MulliganProfile
+    {
+        List<Card.Cards> CardsToKeep = new List<Card.Cards>();
+
+        private readonly List<Card.Cards> WorthySpells = new List<Card.Cards>
+        {
+            
+        };
+
+        public List<Card.Cards> HandleMulligan(List<Card.Cards> choices, Card.CClass opponentClass,
+            Card.CClass ownClass)
+        {
+            bool HasCoin = choices.Count >= 4;
+
+            int flag1=0;//剑鱼 TSC_086
+            int flag2=0;//血帆桨手 CS3_008
+            
+            foreach (Card.Cards card in choices)
+            {
+                if(card==Card.Cards.TSC_086//剑鱼 TSC_086
+                ){flag1+=1;}
+                if(card==Card.Cards.LOOT_033//洞穴探宝者 LOOT_033
+                ){flag1+=1;}
+            }
+
+            foreach (Card.Cards card in choices)
+            {
+                if(card==Card.Cards.CORE_AT_029// 锈水海盗 CORE_AT_029
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.CORE_AT_029))
+                    {
+                        Keep(card,"锈水海盗");
+                    } 
+                }
+                if(card==Card.Cards.TSC_963// 鱼排斗士 TSC_963
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.TSC_963))
+                    {
+                        Keep(card,"鱼排斗士");
+                    } 
+                }
+                if(card==Card.Cards.LOOT_033// 洞穴探宝者 LOOT_033
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.LOOT_033))
+                    {
+                        Keep(card,"洞穴探宝者");
+                    } 
+                }
+                if(card==Card.Cards.DRG_056// 空降歹徒 DRG_056
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.DRG_056))
+                    {
+                        Keep(card,"空降歹徒");
+                    } 
+                }
+                if(card==Card.Cards.GVG_075// 船载火炮 GVG_075
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.GVG_075))
+                    {
+                        Keep(card,"船载火炮");
+                    } 
+                }
+                if(card==Card.Cards.CORE_AT_029// 锈水海盗 CORE_AT_029
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.CORE_AT_029))
+                    {
+                        Keep(card,"锈水海盗");
+                    } 
+                }
+                if(card==Card.Cards.TSC_086// 剑鱼 TSC_086
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.TSC_086))
+                    {
+                        Keep(card,"剑鱼");
+                    } 
+                }
+                if(card==Card.Cards.VAN_NEW1_022&&flag1>0// 恐怖海盗 VAN_NEW1_022
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.VAN_NEW1_022))
+                    {
+                        Keep(card,"恐怖海盗");
+                    } 
+                }
+                if(card==Card.Cards.SCH_623&&flag1>0// 劈砍课程 SCH_623
+                ){
+                    if(!CardsToKeep.Contains(Card.Cards.SCH_623))
+                    {
+                        Keep(card,"劈砍课程");
+                    } 
+                }
+            }
+            return CardsToKeep;
+        }
+
+        private void Keep(Card.Cards id, string log = "")
+        {
+            CardsToKeep.Add(id);
+            if(log != "")
+                Bot.Log(log);
+        }
+
+    }
+}//德：DRUID 猎：HUNTER 法：MAGE 骑：PALADIN 牧：PRIEST 贼：ROGUE 萨：SHAMAN 术：WARLOCK 战：WARRIOR 瞎：DEMONHUNTER
