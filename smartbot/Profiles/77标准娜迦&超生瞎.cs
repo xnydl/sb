f9d88589605045a2e39f35cf4b92242d371b3e38
xnyld <857343134@qq.com>
2022-04-29 00:26:35 +0800

    BAR_310

--- smartbot/Profiles/77标准娜迦&超生瞎.cs
+++ smartbot/Profiles/77标准娜迦&超生瞎.cs
@@ -364,6 +364,10 @@ namespace SmartBotProfiles
 #region 攻击优先 卡牌威胁（通用） 
 
 
+            if (board.MinionEnemy.Any(minion => minion.Template.Id == Card.Cards.BAR_310))
+            {
+                p.OnBoardBoardEnemyMinionsModifiers.AddOrUpdate(Card.Cards.BAR_310, new Modifier(200));
+            }//光沐元素 Lightshower Elemental ID：BAR_310 
             if (board.MinionEnemy.Any(minion => minion.Template.Id == Card.Cards.AV_118))
             {
                 p.OnBoardBoardEnemyMinionsModifiers.AddOrUpdate(Card.Cards.AV_118, new Modifier(200));
