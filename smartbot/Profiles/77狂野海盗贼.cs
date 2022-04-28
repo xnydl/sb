02ef7f32c7372b2e60db5ac3f6cc7d7206435b9e
xnyld <857343134@qq.com>
2022-04-28 10:01:01 +0800

    剑鱼

--- smartbot/Profiles/77狂野海盗贼.cs
+++ smartbot/Profiles/77狂野海盗贼.cs
@@ -456,7 +456,7 @@ namespace SmartBotProfiles
 #endregion
 #region 剑鱼 TSC_086
          if(board.HasCardInHand(Card.Cards.TSC_086)
-         &&board.WeaponFriend != null
+         &&board.WeaponFriend == null
         ){
             p.CastWeaponsModifiers.AddOrUpdate(Card.Cards.TSC_086, new Modifier(-350));
             Bot.Log("剑鱼 -350");
