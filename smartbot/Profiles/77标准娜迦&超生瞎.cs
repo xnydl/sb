2a28d99fb680e04b8a612370766d60f856f77b57
xnyld <857343134@qq.com>
2022-05-01 01:12:01 +0800

    赛丝诺女士

--- smartbot/Profiles/77标准娜迦&超生瞎.cs
+++ smartbot/Profiles/77标准娜迦&超生瞎.cs
@@ -327,6 +327,7 @@ namespace SmartBotProfiles
 
 #region 赛丝诺女士 Lady S'theno ID：TSC_218 
             if(board.HasCardInHand(Card.Cards.TSC_218)
+            &&board.MinionEnemy.Count == 0
             ){
             p.CastMinionsModifiers.AddOrUpdate(Card.Cards.TSC_218, new Modifier(130)); 
             Bot.Log("赛丝诺女士 130");
