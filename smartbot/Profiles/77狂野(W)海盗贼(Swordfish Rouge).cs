eee4a576d02171de0a35e8de687b126c1f07264d
xnyld <857343134@qq.com>
2022-05-05 21:31:13 +0800

    1

--- smartbot/Profiles/77狂野(W)海盗贼(Swordfish Rouge).cs
+++ smartbot/Profiles/77狂野(W)海盗贼(Swordfish Rouge).cs
@@ -459,7 +459,7 @@ namespace SmartBotProfiles
             Bot.Log("转刀");
           }
           if(board.MaxMana ==2
-          &&(!board.HasCardInHand(Card.Cards.TSC_086)&&!board.HasCardInHand(Card.Cards.LOOT_033))
+          &&(!board.HasCardInHand(Card.Cards.TSC_086)&&!board.HasCardInHand(Card.Cards.LOOT_033)&&(!board.HasCardInHand(Card.Cards.GVG_075)&&!board.HasCardInHand(Card.Cards.GAME_005)))
           ){
             p.CastHeroPowerModifier.AddOrUpdate(Card.Cards.HERO_03bp, new Modifier(-99));//匕首精通 Dagger Mastery ID：HERO_03bp 
             p.CastHeroPowerModifier.AddOrUpdate(Card.Cards.HERO_03dbp, new Modifier(-99));//匕首精通 Dagger Mastery ID：HERO_03bp 
