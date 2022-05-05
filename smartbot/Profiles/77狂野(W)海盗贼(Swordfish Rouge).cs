9b179744f274f737dc11d9492d5837ea94725e26
xnyld <857343134@qq.com>
2022-05-05 22:55:39 +0800

    1

--- smartbot/Profiles/77狂野(W)海盗贼(Swordfish Rouge).cs
+++ smartbot/Profiles/77狂野(W)海盗贼(Swordfish Rouge).cs
@@ -365,8 +365,9 @@ namespace SmartBotProfiles
 #endregion
 #region 致命药膏 CORE_CS2_074 
             if(board.HasCardInHand(Card.Cards.CORE_CS2_074)
-            &&board.HasCardInHand(Card.Cards.TSC_086)
-            &&board.WeaponFriend == null
+            &&board.WeaponFriend != null 
+            && board.WeaponFriend.Template.Id == Card.Cards.TSC_086
+            && board.WeaponFriend.CurrentDurability >= 2
             ){
             p.CastSpellsModifiers.AddOrUpdate(Card.Cards.CORE_CS2_074, new Modifier(-99)); 
             Bot.Log("致命药膏 -99");
